4349870b80d81a8e2b711628e38d99d171475cfe
aliakbarvafaei <72415266+aliakbarvafaei@users.noreply.github.com>
2023-01-28 11:34:08 +0330

    Convert typescript to node and remove config typescript

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cookie_parser = require("cookie-parser");
 
 // var spawn = require("child_process").spawn;
 // var process = spawn("python", [`${__dirname}/removeBg.py`, `${__dirname}/1.jpg`, `${__dirname}//output.png`]);
-// process.stdout.on("data", function (data: any) {
+// process.stdout.on("data", function (data) {
 //   console.log(data.toString());
 // });
 //config database
@@ -18,12 +18,12 @@ app.use(express.json({ limit: "50mb" }));
 app.use(cookie_parser());
 
 //route
-fs.readdirSync(`${__dirname}/routes`).map((route: any) => {
+fs.readdirSync(`${__dirname}/routes`).map((route) => {
   app.use("/irnode", require(`./routes/${route}`));
 });
 
 //create server
 const port = 8080;
-let server = app.listen(port, (req: any, res: any) => {
+let server = app.listen(port, (req, res) => {
   console.log(`sever is running on port ${port} ...`);
 });

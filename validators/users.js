9961bfe98aa8d59b6dbdbe45d583f937a60ee85e
aliakbarvafaei <72415266+aliakbarvafaei@users.noreply.github.com>
2023-01-31 19:34:13 +0330

    Complete all validate in users routes

--- validators/users.js
+++ validators/users.js
@@ -3,11 +3,13 @@ const { PrismaClient } = require("@prisma/client");
 const Schema = require("validate");
 const { add } = require("lodash");
 const db = new PrismaClient();
+var jwt = require("jsonwebtoken");
 require("dotenv").config();
 
 //lodash
 const { parseInt } = require("lodash");
 const _ = require("lodash");
+const SECRET = "secret";
 
 exports.vUserCraete = (req, res, next) => {
   try {
@@ -31,7 +33,7 @@ exports.vUserCraete = (req, res, next) => {
       },
       gender: {
         type: String,
-        enum: ["MAN","WOMAN"],
+        enum: ["MAN", "WOMAN"],
         required: true,
       },
       password: {
@@ -42,275 +44,125 @@ exports.vUserCraete = (req, res, next) => {
     });
     var res_data = [];
     const errors = user.validate(req.body);
-    errors.forEach(element => {
-      res_data.push(element.message)
-    })
-    if(res_data.length>0)
-        return res.status(400).send(res_data)
-    const email_validation = require('email-validator')
-    if (!email_validation.validate(req.body.email)){
-        return res.status(400).send('invalid email')
+    errors.forEach((element) => {
+      res_data.push(element.message);
+    });
+    if (res_data.length > 0) return res.status(400).send(res_data);
+    const email_validation = require("email-validator");
+    if (!email_validation.validate(req.body.email)) {
+      return res.status(400).send("invalid email");
     }
     // console.log(req.header("Authorization"));
-    next()
-
+    next();
   } catch (error) {
     console.log(error);
-    return res.status(500).send(
-        "عملیات با خطا مواجه شد",
-    );
+    return res.status(500).send("Internal Server Error");
   }
 };
 
 exports.vUserLogin = (req, res, next) => {
   try {
-    const username = req.body.username;
-    const email = req.body.email;
-    const phone = req.body.phone;
-    const password = req.body.password;
-
-    //validate username
-    if (!username) {
-      return res.json({
-        status: -1,
-        msg: "خطا: نام کاربری وارد نشده است",
-      });
-    }
-
-    //validate password
-    else if (!password) {
-      return res.json({
-        status: -1,
-        msg: "خطا: رمز وارد نشده است",
-      });
-    }
-
-    //validate email & phone
-    else if (!email && !phone) {
-      return res.json({
-        status: -1,
-        msg: "خطا: حداقل باید یکی از دو قسمت ایمیل یا شماره تلفن را وارد کنید",
-      });
+    const user = new Schema({
+      email: {
+        type: String,
+        required: true,
+      },
+      password: {
+        type: String,
+        required: true,
+        length: { min: 8 },
+      },
+    });
+    var res_data = [];
+    const errors = user.validate(req.body);
+    errors.forEach((element) => {
+      res_data.push(element.message);
+    });
+    if (res_data.length > 0) return res.status(400).send(res_data);
+    const email_validation = require("email-validator");
+    if (!email_validation.validate(req.body.email)) {
+      return res.status(400).send("invalid email");
     }
 
     next();
   } catch (error) {
     console.log(error);
-    return res.json({
-      status: -1,
-      msg: "خطا: عملیات با خطا مواجه شد",
-    });
+    return res.status(500).send("Internal Server Error");
   }
 };
 
 exports.vUserUpdate = async (req, res, next) => {
   try {
-    const id = parseInt(req.body.id);
-    const old_password = req.body.oldPassword;
-    const new_username = req.body.username;
-    const new_firstName = req.body.firstName;
-    const new_lastName = req.body.lastName;
-    const new_email = req.body.email;
-    const new_phone = req.body.phone;
-
-    //check exist user
-    let user = await db.User.findFirst({
-      where: {
-        id: id,
+    const user = new Schema({
+      firstname: {
+        type: String,
+      },
+      lastname: {
+        type: String,
+      },
+      email: {
+        type: String,
+      },
+      phone: {
+        type: Number,
+      },
+      age: {
+        type: Number,
+      },
+      gender: {
+        type: String,
+        enum: ["MAN", "WOMAN"],
+      },
+      password: {
+        type: String,
+        length: { min: 8 },
       },
     });
-    if (!user) {
-      return res.json({
-        status: -1,
-        msg: "خطا: کاربری با این آی‌دی وجود ندارد",
-      });
-    }
-
-    //authentication old password
-    if (!old_password) {
-      return res.json({
-        status: -1,
-        msg: "خطا: وارد کردن رمز عبور قبلی الزامی است",
-      });
-    }
-
-    user_auth = await db.User.findFirst({
-      where: {
-        id: id,
-        password: old_password,
-      },
+    var res_data = [];
+    const errors = user.validate(req.body);
+    errors.forEach((element) => {
+      res_data.push(element.message);
     });
-    if (!user_auth) {
-      return res.json({
-        status: -1,
-        msg: "خطا: رمز عبور صحیح نمی‌باشد",
-      });
+    if (res_data.length > 0) return res.status(400).send(res_data);
+    const email_validation = require("email-validator");
+    if (req.body.email && !email_validation.validate(req.body.email)) {
+      return res.status(400).send("invalid email");
     }
-
-    //check duplicate username
-    let duplicate_user = await db.User.findFirst({
-      where: {
-        NOT: {
-          id: id,
-        },
-        username: new_username,
-      },
-    });
-    if (duplicate_user) {
-      return res.json({
-        status: -1,
-        msg: "خطا: نام کاربری تکراری می‌باشد",
-      });
+    try {
+      jwt.verify(req.header("Authorization"), SECRET);
+    } catch {
+      return res.status(400).send("invalid token.");
     }
-
-    //check email or phone
-    if (!new_email && !new_phone) {
-      return res.json({
-        status: -1,
-        msg: "خطا: حداقل باید یکی از دو قسمت ایمیل یا شماره تلفن را وارد کنید",
-      });
-    }
-
-    //validate email
-    else if (new_email) {
-      if (new_email.indexOf("@") < 1) {
-        return res.json({
-          status: -1,
-          msg: "خطا: ایمیل شما صحیح نمی‌باشد",
-        });
-      }
-      if (!_.endsWith(new_email, ".com")) {
-        return res.json({
-          status: -1,
-          msg: "خطا: ایمیل باید با .com تمام شود",
-        });
-      }
-    }
-
-    //check duplicate email
-    let duplicate_email = await db.User.findFirst({
-      where: {
-        NOT: {
-          id: id,
-        },
-        email: new_email,
-      },
-    });
-    if (duplicate_email) {
-      return res.json({
-        status: -1,
-        msg: "خطا: ایمیل تکراری می‌باشد",
-      });
-    }
-
-    //validate phone
-    else if (new_phone) {
-      if (!(new_phone.length === 11)) {
-        return res.json({
-          status: -1,
-          msg: "خطا: شماره تلفن باید ۱۱ رقم داشته باشد",
-        });
-      }
-      if (isNaN(new_phone)) {
-        return res.json({
-          status: -1,
-          msg: "خطا: شماره تلفن باید عدد باشد",
-        });
-      }
-    }
-
-    //check duplicate phone
-    let duplicate_phone = await db.User.findFirst({
-      where: {
-        NOT: {
-          id: id,
-        },
-        phone: new_phone,
-      },
-    });
-    if (duplicate_phone) {
-      return res.json({
-        status: -1,
-        msg: "خطا: شماره تلفن تکراری می‌باشد",
-      });
-    }
-
-    //validate firstName
-    else if (_.isNumber(new_firstName)) {
-      return res.json({
-        status: -1,
-        msg: "نام نباید شامل عدد باشد",
-      });
-    }
-
-    //validate lastName
-    else if (_.isNumber(new_lastName)) {
-      return res.json({
-        status: -1,
-        msg: "نام خانوادگی نباید شامل عدد باشد",
-      });
-    }
-
-    next();
   } catch (error) {
     console.log(error);
-    return res.json({
-      status: -1,
-      msg: "خطا: عملیات با خطا مواجه شد",
-    });
+    return res.status(500).send("Internal Server Error");
   }
 };
 
 exports.vUserDelete = async (req, res, next) => {
   try {
-    //validate id parameter in url
-    id = req.params.userId;
-    if (isNaN(id)) {
-      return res.json({
-        status: -1,
-        msg: "خطا: آی‌دی باید به صورت عددی باشد",
-      });
+    try {
+      jwt.verify(req.header("Authorization"), SECRET);
+    } catch {
+      return res.status(400).send("invalid token.");
     }
-
-    //check isExist user
-    let user = await db.User.findFirst({
-      where: {
-        id: parseInt(id),
-      },
-    });
-    if (!user) {
-      return res.json({
-        status: -1,
-        msg: "خطا: کاربری با این آی‌دی وجود ندارد",
-      });
-    }
-
     next();
   } catch (error) {
     console.log(error);
-    return res.json({
-      status: -1,
-      msg: "خطا: عملیات با خطا مواجه شد",
-    });
+    return res.status(500).send("Internal Server Error");
   }
 };
 
 exports.vGetOne = (req, res, next) => {
   try {
-    //validate id parameter in url
-    id = req.params.userId;
-    if (isNaN(id)) {
-      return res.json({
-        status: -1,
-        msg: "خطا: آی‌دی باید به صورت عددی باشد",
-      });
+    try {
+      jwt.verify(req.header("Authorization"), SECRET);
+    } catch {
+      return res.status(400).send("invalid token.");
     }
     next();
   } catch (error) {
     console.log(error);
-    return res.json({
-      status: -1,
-      msg: "خطا: عملیات با خطا مواجه شد",
-    });
+    return res.status(500).send("Internal Server Error");
   }
 };

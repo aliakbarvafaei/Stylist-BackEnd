d7d7a7134e047e8097dcc36460ce7538f90c7efc
aliakbarvafaei <72415266+aliakbarvafaei@users.noreply.github.com>
2023-02-23 10:49:26 +0330

    Comment sendSms and log code

--- functions/sms.js
+++ functions/sms.js
@@ -1,29 +1,30 @@
 const axios = require("axios");
 
 exports.sendSms = (reciver, code) => {
-  var data = JSON.stringify({
-    lineNumber: 30007732008851,
-    messageText: `به استایلست خوش آمدید.\nکد ارسالی: ${code}`,
-    mobiles: [reciver],
-    sendDateTime: null,
-  });
+  console.log("sms code: " + code);
+  // var data = JSON.stringify({
+  //   lineNumber: 30007732008851,
+  //   messageText: `به استایلست خوش آمدید.\nکد ارسالی: ${code}`,
+  //   mobiles: [reciver],
+  //   sendDateTime: null,
+  // });
 
-  var config = {
-    method: "post",
-    url: "https://api.sms.ir/v1/send/bulk",
-    headers: {
-      "X-API-KEY":
-        "XLdop743ieewpDZ2q9zCj4Vp5Zjf8o5LzCZahrAbXovI5ftdo1dzC6UXphgfQOZm",
-      "Content-Type": "application/json",
-    },
-    data: data,
-  };
+  // var config = {
+  //   method: "post",
+  //   url: "https://api.sms.ir/v1/send/bulk",
+  //   headers: {
+  //     "X-API-KEY":
+  //       "XLdop743ieewpDZ2q9zCj4Vp5Zjf8o5LzCZahrAbXovI5ftdo1dzC6UXphgfQOZm",
+  //     "Content-Type": "application/json",
+  //   },
+  //   data: data,
+  // };
 
-  axios(config)
-    .then(function (response) {
-      console.log(response.data);
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
+  // axios(config)
+  //   .then(function (response) {
+  //     console.log(response.data);
+  //   })
+  //   .catch(function (error) {
+  //     console.log(error);
+  //   });
 };
